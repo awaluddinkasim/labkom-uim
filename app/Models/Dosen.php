4ede461a92e31f5c0d0f03dfe595bdff9d197d45
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2022-07-12 20:15:04 +0800

    remove unimported use

--- app/Models/Dosen.php
+++ app/Models/Dosen.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Dosen extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
