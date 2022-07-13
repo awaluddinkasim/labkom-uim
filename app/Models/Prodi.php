d8dd974ead9018e67d829880e936de676528dd10
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2022-07-13 15:18:48 +0800

    prodi model fakultas & mahasiswa relation

--- app/Models/Prodi.php
+++ app/Models/Prodi.php
@@ -10,4 +10,14 @@ class Prodi extends Model
     use HasFactory;
 
     protected $table = 'prodi';
+
+    public function fakultas()
+    {
+        return $this->belongsTo(Fakultas::class, 'id_fakultas');
+    }
+
+    public function mahasiswa()
+    {
+        return $this->hasMany(User::class, 'id_prodi');
+    }
 }
