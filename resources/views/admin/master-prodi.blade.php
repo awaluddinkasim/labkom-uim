b20a8a80795c66cb5f6f891dcad07d876dc5592f
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2022-07-13 15:15:14 +0800

    master data prodi view update

--- resources/views/admin/master-prodi.blade.php
+++ resources/views/admin/master-prodi.blade.php
@@ -10,7 +10,52 @@
         </div>
 
         <div class="section-body">
-
+            <div class="card">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Fakultas</th>
+                                    <th>Program Studi</th>
+                                    <th>Mahasiswa Terdaftar</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($daftarJurusan as $prodi)
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td></td>
+                                    <td>{{ $prodi->nama }}</td>
+                                    <td></td>
+                                    <td class="text-center">
+                                        <button class="btn btn-info btn-sm">
+                                            <ion-icon name="create"></ion-icon>
+                                        </button>
+                                        <form action="" class="d-inline" method="POST">
+                                            @csrf
+                                            @method('DELETE')
+                                            <input type="hidden" name="id" value="{{ $prodi->id }}">
+                                            <button class="btn btn-danger btn-sm" type="submit">
+                                                <ion-icon name="trash"></ion-icon>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @empty
+                                <tr>
+                                    <td colspan="5" class="text-center">
+                                        <i>Tidak ada data</i>
+                                    </td>
+                                </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
 @endsection
