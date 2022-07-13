a62667538048a76d8ecf4d905baa5ca1a1de66db
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2022-07-13 15:52:43 +0800

    model fakultas relation to prodi

--- app/Models/Fakultas.php
+++ app/Models/Fakultas.php
@@ -10,4 +10,9 @@ class Fakultas extends Model
     use HasFactory;
 
     protected $table = "fakultas";
+
+    public function prodi()
+    {
+        return $this->hasMany(Prodi::class, 'id_fakultas');
+    }
 }
