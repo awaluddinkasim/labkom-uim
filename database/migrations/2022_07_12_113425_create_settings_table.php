118430b1f1adfb255f65b5ba6588e0114c242bbd
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2022-07-14 10:11:29 +0800

    settings model & migration initial value

--- database/migrations/2022_07_12_113425_create_settings_table.php
+++ database/migrations/2022_07_12_113425_create_settings_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Setting;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,6 +20,21 @@ return new class extends Migration
             $table->string('value');
             $table->timestamps();
         });
+
+        Setting::insert([
+            [
+                'name' => 'semester',
+                'value' => 'ganjil',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'upload',
+                'value' => 'buka',
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+        ]);
     }
 
     /**
