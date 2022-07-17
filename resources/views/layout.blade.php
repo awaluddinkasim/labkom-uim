ca018bbe6209768fb9e17562a182947e404f1a20
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2022-07-17 15:27:14 +0800

    favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ config('app.name') }}</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     @include('includes.styles')
     @stack('styles')
 </head>
