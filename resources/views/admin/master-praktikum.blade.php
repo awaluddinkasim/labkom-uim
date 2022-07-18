96cca634152bdd2c253cc12a5abab356f0c25541
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2022-07-18 15:07:18 +0800

    master data failed alert

--- resources/views/admin/master-praktikum.blade.php
+++ resources/views/admin/master-praktikum.blade.php
@@ -17,6 +17,14 @@
             </button>
         </div>
         @endif
+        @if (Session::has('failed'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            {{ Session::get('failed') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        @endif
 
         <div class="section-body">
             <div class="card">
