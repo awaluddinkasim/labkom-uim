eb197c0fc4874b1cbbc30f6346084727d3ec9fd8
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2022-07-18 16:30:17 +0800

    daftar jurusan

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Fakultas;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
+    public function daftarJurusan()
+    {
+        $data = [
+            'daftarJurusan' => Fakultas::orderBy('nama')->get()
+        ];
 
+        return response()->json($data, 200);
+    }
 }
